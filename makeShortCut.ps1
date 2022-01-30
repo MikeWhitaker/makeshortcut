22aba6f7c0ed5dd9a6b9de38ed707b4f31e79cd8
Michael Whitaker <whitaker.mj@gmail.com>
2022-01-30 09:09:12 +0100

    Modified gitignore file MakeShortCut now check of null parameter

--- makeShortCut.ps1
+++ makeShortCut.ps1
@@ -1,4 +1,8 @@
 $nameOfShortCut = $Args[0]
+if (!$nameOfShortCut ) {
+    write-host "Error: no name of shortcut provided -> Quit ..."
+    exit
+}
 $currentFolder = $pwd.Path
 #first make a file with the name from the parameter.
 Add-Content "C:\runShortCuts\go$nameOfShortCut.bat" "cd $currentFolder"
